3dd2e80ca3dcd7d32223ac83b85c5835737a166f
kaashntr <kaashntr@github.com>
2025-06-04 19:52:57 +0300

    Security group module

---
+++ terraform/modules/nsg/variables.tf
@@ -0,0 +1,13 @@
+variable "resource_group_name" {
+  type        = string
+}
+
+variable "location" {
+  type        = string
+  description = "Region Azure"
+}
+
+variable "prefix" {
+  type        = string
+  description = "Resourse's prefix"
+}
